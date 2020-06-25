267b2095bae920f325010a22fde8d906b20f3198
Stephanie Hales <stephleehales@gmail.com>
2020-06-25 13:08:17 +1000

    first commit - react CRUD lesson

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import blogData from './data/post_data'
+import Nav from './components/Nav'
+import BlogPosts from './components/BlogPosts'
+import BlogPost from './components/BlogPost'
+import NewBlogPost from './components/NewBlogPost'
 
 const App = () => {
+  const [blogPosts, setBlogposts] = useState([])
+
+  useEffect(()=>{
+    setBlogposts(blogData)
+  }, [])
+
+  function getPostFromId (id) {
+    //console.log(blogPosts)
+    return blogPosts.find((post) => post._id === parseInt(id) )
+  }
+
+  function addBlogPost(post){
+    setBlogposts([...blogPosts,post])
+  }
+
+  function getNextId(){
+    const ids = blogPosts.map((post)=> post._id)
+    return ids.sort()[ids.length - 1] + 1
+  }
+
   return (
     <div >
-          The basic empty template
+      <BrowserRouter>
+          <Nav />
+          <h1>The Strong Coder </h1>
+          <Switch>
+          <Route exact path="/" render={(props) => <BlogPosts {...props} postData={blogPosts} /> }  />
+          <Route exact path="/posts/new" render={(props)=> 
+            <NewBlogPost {...props} addBlogPost={addBlogPost} nextId={getNextId()} /> } />
+          <Route exact path="/posts/:id" render={(props) => 
+            <BlogPost {...props} post={getPostFromId(props.match.params.id)} /> } />
+        </Switch> 
+        </BrowserRouter>
     </div>
   )
 }
